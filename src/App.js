413d954871f7731140d898f9440a7699e693a893
God3ly <dhanushshetty201@gmail.com>
2022-06-17 01:46:51 +0530

    Loading Animation Added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -6,17 +6,34 @@ import Skills from "./components/Skills";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 
 function App() {
+  let [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
+
   return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-      <Skills />
-      <Work />
-      <Contact />
-      <Footer />
+    <div className="App">
+      {loading ? (
+        <div className="flex flex-col justify-center items-center h-screen w-screen  bg-[#0a192f]">
+          <ClimbingBoxLoader color="#e2196c" loading={loading} size={25} />
+        </div>
+      ) : (
+        <div>
+          <NavBar />
+          <Home />
+          <About />
+          <Skills />
+          <Work />
+          <Contact />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }

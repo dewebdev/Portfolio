f63dcc7be1dbe3eca36193198fb6816fbd437f5b
God3ly <dhanushshetty201@gmail.com>
2022-07-05 11:21:23 +0530

    Loading animation Removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -6,34 +6,34 @@ import Skills from "./components/Skills";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import HashLoader from "react-spinners/HashLoader";
+// import HashLoader from "react-spinners/HashLoader";
 
 function App() {
-  let [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
+  // let [loading, setLoading] = useState(false);
+  // useEffect(() => {
+  //   setLoading(true);
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 2000);
+  // }, []);
 
   return (
     <div className="App">
-      {loading ? (
+      {/* {loading ? (
         <div className="flex flex justify-center items-center h-screen w-screen  bg-[#0a192f]">
           <HashLoader color="#e2196c" loading={loading} size={200} />
         </div>
-      ) : (
-        <div>
-          <NavBar />
-          <Home />
-          <About />
-          <Skills />
-          <Work />
-          <Contact />
-          <Footer />
-        </div>
-      )}
+      ) : ( */}
+      {/* <div> */}
+      <NavBar />
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
+      <Footer />
+      {/* </div>
+      )} */}
     </div>
   );
 }
